25ba231ac1508c698c27650bb4e5012ab85fb2bb
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2022-11-08 00:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hế nhô bạn hiền!🙋‍♂️🙋‍♂️',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'Chúng con thay mặt lớp 11TH1 niên khóa 21-24 kính chào thầy ạ!🙋‍♂️🙋‍♂️',
+    introDesc: `Chúng con đố thầy một câu hỏi, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu có thấy tớ đẹp trai hong nè 😙',
+    title: 'Hôm nay là ngày gì dị thầy 😙',
     desc: 'Cậu không trả lời hoặc thoát ra, tức là tớ là người đẹp trai nhất cậu từng gặp đó nha 😉🤞😘',
-    btnYes: 'Cóooo😚🙄',
-    btnNo: 'Có ăn cứtttt💩💩💩',
-    question:'Nói cho tớ đẹp trai ở đâu điiii',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai, hài hước hihi 😋',
-    mess: 'Tớ biết mà 🥰. Tớ thấy cậu cũng xinh lắm á😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
-    btnAccept: 'Okiiiii lun ❤🧡💛💙💚💜🖤💖😎',
-    messLink: 'https://www.facebook.com/profile.php?id=100034444426000' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Ngày giáo Việt Nam😚🙄',
+    btnNo: 'Ngày chủ nhật thoải mái',
+    question:'Đúng vậy! Nên vì thế thầy hãy gõ vào bàn phím để nhận điều bất ngờ nhé!!',
+    btnReply: 'Sau khi gõ thì ấn vào đây nè',
+    reply: 'Chúng con chúc thầy ngày 20.11 mãi trẻ, mãi khỏe và đạt được những điều mình muốn trong cuộc sống hihi 😋',
+    mess: 'Happy teacher's day😘😘',
+    messDesc: 'Thầy cô Trần Chuyên mãi đỉnh',
+    btnAccept: 'Mãi iu thầy cô ❤🧡💛💙💚💜🖤💖😎',
+    messLink: 'https://www.youtube.com/watch?v=g6bCerSztIk' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
