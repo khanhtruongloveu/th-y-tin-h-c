b5a294dadc8075dca78471bae55e8d2ef7b16de3
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2022-11-07 23:50:59 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hế nhô bạn hiền!🙋‍♂️🙋‍♂️',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Cậu có thấy tớ đẹp trai hong nè 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là tớ là người đẹp trai nhất cậu từng gặp đó nha 😉🤞😘',
+    btnYes: 'Cóooo😚🙄',
+    btnNo: 'Có ăn cứtttt💩💩💩',
+    question:'Nói cho tớ đẹp trai ở đâu điiii',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì cậu đẹp trai, hài hước hihi 😋',
+    mess: 'Tớ biết mà 🥰. Tớ thấy cậu cũng xinh lắm á😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun ❤🧡💛💙💚💜🖤💖😎',
+    messLink: 'https://www.facebook.com/profile.php?id=100034444426000' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
